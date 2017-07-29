10bcc1f606bcb04c19d68c6a121af3211638443a
Daniel Perez <dperez@localhost.localdomain>
2017-07-28 23:24:45 -0500

    first commit with firebase

--- listTask.js
+++ listTask.js
@@ -1,29 +1,53 @@
- angular.module('todoAp', [])
-  .controller('ControladorTareas', ['$scope', function($scope) {
-    
+angular.module('todoAp', ['firebase'])
+  .controller('ControladorTareas', ['$scope', '$firebaseArray', '$filter', function ($scope, $firebaseArray, $filter) {
+
     var ctrl = this;
-    
-  ctrl.tareas = [{texto: 'Ser Super Heroico con AngularJS', hecho: true},
-                   {texto: 'Crear una ap con angular', hecho: false}];
-                   
-  ctrl.agregarTarea = function() {
-    ctrl.tareas.push({texto: ctrl.textoNuevaTarea, hecho: false});
-    ctrl.textoNuevaTarea = '';
-  };
+    var config = {
+      apiKey: "AIzaSyAGMG3kqRqPYgcJyPVJRddOrctZg-JZF9k",
+      authDomain: "listtask-44e8c.firebaseapp.com",
+      databaseURL: "https://listtask-44e8c.firebaseio.com",
+      projectId: "listtask-44e8c",
+      storageBucket: "",
+      messagingSenderId: "475529079338"
+    };
+    firebase.initializeApp(config);
+    var ref = firebase.database().ref();
 
-  ctrl.restantes = function() {
-    var cuenta = 0;
-    angular.forEach(ctrl.tareas, function(tarea) {
-      cuenta += tarea.hecho ? 0 : 1;
-    });
-    return cuenta;
-  };
+    ctrl.tareas = $firebaseArray(ref);
 
-  ctrl.eliminar = function() {
-    var tareasViejas = ctrl.tareas;
-    ctrl.tareas = [];
-    angular.forEach(tareasViejas, function(tarea) {
-      if (!tarea.hecho) ctrl.tareas.push(tarea);
+    ctrl.tareas.$loaded().then(function (tareas) {
+      console.log(tareas.length); // data is loaded here
     });
-  };
-}]);
+
+    ctrl.agregarTarea = function () {
+      ctrl.tareas.$add({ texto: ctrl.textoNuevaTarea, hecho: false });
+      ctrl.textoNuevaTarea = '';
+    };
+
+    ctrl.restantes = function () {
+      var cuenta = 0;
+      angular.forEach(ctrl.tareas, function (tarea) {
+          cuenta++;
+      })
+      return cuenta;
+    };
+
+    ctrl.tareasRealizadas = function () {
+      var cuenta = 0;
+      angular.forEach(ctrl.tareas, function (tarea) {
+        if (tarea.hecho) {
+          cuenta++;
+        }
+      })
+      return cuenta;
+    }
+
+    ctrl.eliminar = function () {
+      angular.forEach(ctrl.tareas, function (tarea) {
+        if (tarea.hecho) {
+          ctrl.tareas.$remove(tarea).then(function (ref) {
+          });
+        }
+      });
+    };
+  }]);
